41582c2ede0cb50f539a65768ecbd31196b2d637
bwhetstonestdbev <brandon.whetstone@stdbev.com>
2025-11-12 13:10:38 -0600

    Add files via upload

---
+++ CreateLocalAdmin.ps1
@@ -0,0 +1,4 @@
+﻿$username = 'SBCAdmin'
+$password = ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force
+New-LocalUser -Name $username -Password P@ssw0rd -FullName $username -Description 'Local Admin Account' 
+Add-LocalGroupMember -Group "Administrators" -Member $username
---
+++ JoinDomain.ps1
@@ -0,0 +1,5 @@
+﻿$SecurePassword = ConvertTo-SecureString "!Ntun3dem" -AsPlainText -Force
+
+$Creds = New-Object System.Management.Automation.PSCredential ("intune.dem",$SecurePassword)
+
+Add-Computer -DomainName stdbev.com -Credential $Creds -OUPath "OU=Computers - STDBEV,DC=stdbev,DC=com" -Restart
