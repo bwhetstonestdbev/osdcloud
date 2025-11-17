2c9c71db1710534f8447ac83e07057827e87cb6c
bwhetstonestdbev <brandon.whetstone@stdbev.com>
2025-11-17 16:26:56 -0600

    Update JoinDomain.ps1

--- JoinDomain.ps1
+++ JoinDomain.ps1
@@ -5,8 +5,9 @@ Start-Transcript -Path $logFile -NoClobber
 $VerbosePreference = "Continue"
  
 $SecurePassword = ConvertTo-SecureString "!Ntun3dem" -AsPlainText -Force
-$Creds = New-Object System.Management.Automation.PSCredential ("intune.dem",$SecurePassword)
+$Creds = New-Object System.Management.Automation.PSCredential ("stdbev.com\intune.dem",$SecurePassword)
 Add-Computer -DomainName stdbev.com -Credential $Creds -OUPath "OU=Computers - STDBEV,DC=stdbev,DC=com" -Restart
 
 Stop-Transcript
+
 
