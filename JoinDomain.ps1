56f4f6ad870ba2b6e746fc327e57cb61ab3979ba
bwhetstonestdbev <brandon.whetstone@stdbev.com>
2025-11-17 15:33:21 -0600

    Update JoinDomain.ps1

--- JoinDomain.ps1
+++ JoinDomain.ps1
@@ -1,5 +1,14 @@
-﻿$SecurePassword = ConvertTo-SecureString "!Ntun3dem" -AsPlainText -Force
+$stampDate = Get-Date
+$scriptName = ([System.IO.Path]::GetFileNameWithoutExtension($(Split-Path $script:MyInvocation.MyCommand.Path -Leaf)))
+$logFile = "$env:OSDCloud\Logs\$scriptName-" + $stampDate.ToFileTimeUtc() + ".log"
+Start-Transcript -Path $logFile -NoClobber
+$VerbosePreference = "Continue"
+ 
+ $SecurePassword = ConvertTo-SecureString "!Ntun3dem" -AsPlainText -Force
 
 $Creds = New-Object System.Management.Automation.PSCredential ("intune.dem",$SecurePassword)
 
+
 Add-Computer -DomainName stdbev.com -Credential $Creds -OUPath "OU=Computers - STDBEV,DC=stdbev,DC=com" -Restart
+
+Stop-Transcript
