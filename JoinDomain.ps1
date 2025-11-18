fcab290aebbf7e342a31f26e7ca75bd422e80ce5
bwhetstonestdbev <brandon.whetstone@stdbev.com>
2025-11-18 10:12:01 -0600

    Update JoinDomain.ps1

--- JoinDomain.ps1
+++ JoinDomain.ps1
@@ -8,12 +8,14 @@ $input = Get-Content -Path $env:SystemDrive\OSDCloud\Scripts\Name.txt
 Write-Host -ForegroundColor Red "Rename Computer before Domain Join"
 $Serial = Get-WmiObject Win32_bios | Select-Object -ExpandProperty SerialNumber
 $ComputerName = 'D' + $Serial + '-' + $input
- 
-$SecurePassword = ConvertTo-SecureString "!Ntun3dem" -AsPlainText -Force
+
+$pass = Get-Content -Path $env:SystemDrive\OSDCloud\Scripts\pass.txt 
+$SecurePassword = ConvertTo-SecureString $pass -AsPlainText -Force
 $Creds = New-Object System.Management.Automation.PSCredential ("stdbev.com\intune.dem",$SecurePassword)
 Add-Computer -DomainName stdbev.com -Credential $Creds -OUPath "OU=Computers - STDBEV,DC=stdbev,DC=com" -NewName $ComputerName -Force -Restart
 
 Stop-Transcript
+
 
 
 
