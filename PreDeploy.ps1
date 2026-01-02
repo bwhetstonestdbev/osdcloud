8a66afe9f4632563b1d6ba6264db1483d9807fb2
bwhetstonestdbev <brandon.whetstone@stdbev.com>
2026-01-02 09:46:10 -0600

    Update PreDeploy.ps1

--- PreDeploy.ps1
+++ PreDeploy.ps1
@@ -12,7 +12,7 @@ $VerbosePreference = "Continue"
 #########################
 $pass = Get-Content -Path $env:SystemDrive\OSDCloud\Scripts\pass.txt 
 $SecurePassword = ConvertTo-SecureString $pass -AsPlainText -Force
-$Creds = New-Object System.Management.Automation.PSCredential ("stdbev.com\sbc.imaging",$SecurePassword)
+$Creds = New-Object System.Management.Automation.PSCredential ("stdbev.com\intune.dem",$SecurePassword)
 
 #########################
 # Set Default Taskbar Settings
@@ -23,39 +23,18 @@ New-itemproperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explor
 REG UNLOAD HKLM\Default
 
 #########################
-# Create the computer name and store it on ADSync Server so we can access it later
+# Create the computer name
 #########################
-$sourcePath = "\\sbc365adsync01\OSDCloud\"
-New-PSDrive -Name "Q" -PSProvider FileSystem -Root $sourcePath -Credential $Creds -ErrorAction Stop
-
 $input = Get-Content -Path $env:SystemDrive\OSDCloud\Scripts\Name.txt
 Write-Host -ForegroundColor Red "Rename Computer before Domain Join"
 $Serial = Get-WmiObject Win32_bios | Select-Object -ExpandProperty SerialNumber
 $computerName = $Serial + '-' + $input
-$computerName | Out-File -FilePath "Q:\${computerName}_log.txt" -Encoding ascii -Force
-
-Remove-PSDrive -Name Q
 
 #########################
-# See if computer exists in AD. If it does, remove it. Must run remote command on server with AD Powershell commands installed
+# Add Computer to AD
 #########################
-Enable-WSManCredSSP -Role Client -DelegateComputer SBC365ADSYNC01.stdbev.com -Force
-$session = New-PSSession -cn SBC365ADSYNC01.stdbev.com -Credential $Creds -Authentication Credssp
-Invoke-Command -Session $session -ScriptBlock {C:\OSDCloud\CheckADComputer.ps1}
+$organizationalUnit = "OU=Computers - STDBEV,DC=stdbev,DC=com"
 
 Add-Computer -DomainName stdbev.com -Credential $Creds -OUPath $organizationalUnit -NewName $computerName -Force -Restart
 
 Stop-Transcript
-
-
-
-
-
-
-
-
-
-
-
-
-
